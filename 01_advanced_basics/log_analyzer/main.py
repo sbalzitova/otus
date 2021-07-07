8b29cd48829e5a085feebd3bb25bc4368d5f41b2
Sveta Balzitova <sbalzitova@yandex-team.ru>
2021-07-07 16:36:43 +0300

    removed classes

--- 01_advanced_basics/log_analyzer/main.py
+++ 01_advanced_basics/log_analyzer/main.py
@@ -1,9 +1,20 @@
-from os import path
-from src.report_builder import ReportBuilder
-import argparse
-from json.decoder import JSONDecodeError
+import re
 import json
+import gzip
+import logging
+import datetime
+import argparse
+from string import Template
+from os import path, listdir, makedirs
+from json.decoder import JSONDecodeError
+from src.statistics_builder import StatisticsBuilder
 
+
+logging.basicConfig(filename='script_log.txt',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%Y.%m.%d %H:%M:%S',
+                    level=logging.INFO)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, help='Config file for your log report_builder')
@@ -12,29 +23,56 @@ args = parser.parse_args()
 
 def parse_config(config_file):
     path_to_config = path.join(path.dirname(__file__), config_file)
-    path_to_default_config = path.join(path.dirname(__file__), 'default_config.json')
+
+    default_config = {
+        "REPORT_SIZE": 1000,
+        "REPORT_DIR": "./reports",
+        "LOG_DIR": "./log"
+    }
 
     def open_config_file(file):
         with open(file, 'r') as d:
             try:
                 config_json = json.load(d)
             except JSONDecodeError:
-                config_json = {}
+                return None
         return config_json
 
     config = open_config_file(path_to_config)
-    default_config = open_config_file(path_to_default_config)
+    default_config.update(config)
+    return default_config
 
-    if not config.get('REPORT_SIZE'):
-        config['REPORT_SIZE'] = default_config['REPORT_SIZE']
 
-    if not config.get('REPORT_DIR'):
-        config['REPORT_DIR'] = default_config['REPORT_DIR']
+def find_latest_file(file_dir):
+    max_date = None
+    max_file_name = None
+    report_date = None
 
-    if not config.get('LOG_DIR'):
-        config['LOG_DIR'] = default_config['LOG_DIR']
+    for file_name in listdir(file_dir):
+        matched_file = re.match(r'^nginx-access-ui\.log-(?P<date>\d{8})(\.gz)?$', file_name)
+        if matched_file:
+            report_date = matched_file.groupdict()['date']
 
-    return config
+            if max_date:
+                if report_date and report_date > max_date:
+                    max_date = report_date
+                    max_file_name = file_name
+
+            else:
+                max_date = report_date
+                max_file_name = file_name
+
+    if max_date:
+        report_date = datetime.datetime.strptime(max_date, '%Y%m%d').date()
+
+    return max_file_name, report_date
+
+
+def read_file(file, log_dir):
+    file_dir = path.join(path.dirname(__file__), log_dir, file)
+    file_opener = gzip.open(file_dir, 'r') if file.endswith('.gz') else open(file_dir, 'r')
+    for line in file_opener:
+        yield line.strip().split()
 
 
 def parse_log(log_generator):
@@ -59,31 +97,58 @@ def parse_log(log_generator):
     return url_time_map, parsed_log_percentage
 
 
+def form_report(statistics, report_date, report_dir, report_size):
+    makedirs(report_dir, exist_ok=True)
+
+    if statistics.data:
+        ordered_data = sorted(statistics.data.values(), key=lambda i: i['time_avg'], reverse=True)
+        report_file = path.join(report_dir, 'report-{}.html'.format(report_date))
+
+        with open('report.html', 'r') as source:
+            text = source.read()
+            template = Template(text)
+
+            with open(report_file, 'w+') as target:
+                target.write(template.safe_substitute(table_json=ordered_data[:report_size]))
+
+
 def main():
     processed_config = parse_config(args.config)
-    report_builder = ReportBuilder(processed_config)
 
-    log_generator = report_builder.read_file()
+    if not processed_config:
+        logging.error('Config is corrupted')
+        return
+
+    log_dir = processed_config.get('LOG_DIR')
+    report_size = processed_config.get('REPORT_SIZE')
+    report_dir = processed_config.get('REPORT_DIR')
+
+    log_name, log_date = find_latest_file(log_dir)
+
+    statistics = StatisticsBuilder()
+
+    log_generator = read_file(log_name, log_dir)
 
     is_report_created = path.isfile(path.join(path.dirname(__file__),
-                                              report_builder.report_dir,
-                                              'report-{}.html'.format(report_builder.reader.report_date)))
+                                              report_dir,
+                                              'report-{}.html'.format(log_date)))
 
     if not is_report_created:
         url_time, parsed_percentage = parse_log(log_generator)
 
         if parsed_percentage > 80:
-            print('{}% of the log cant be processed, check if format changed or file missed'.format(parsed_percentage))
+            logging.error('{}% of the log cant be processed, check if format changed '
+                          'or file missed'.format(parsed_percentage))
 
         else:
             for url_time_pair in url_time:
-                report_builder.statistics.register_url(*url_time_pair)
+                statistics.register_url(*url_time_pair)
 
-        report_builder.form_report()
+        form_report(statistics, log_date, report_dir, report_size)
+        logging.info('Analysis completed')
 
     else:
-        print('Report for {} has already been created, check {}'.format(report_builder.reader.report_date,
-                                                                        report_builder.report_dir))
+        logging.info('Report for {} has already been created, check {}'.format(log_date, log_dir))
 
 
 if __name__ == '__main__':
--- 01_advanced_basics/log_analyzer/tests/test_reader.py
+++ 01_advanced_basics/log_analyzer/tests/test_reader.py
@@ -1,23 +1,20 @@
 import unittest
-from src.reader import LogReader
+from main import find_latest_file
 
 
 class TestReader(unittest.TestCase):
 
     def test_date(self):
-        reader = LogReader('tests/test_data/reader/test_date')
-        file = reader.find_latest_file()
+        file, date = find_latest_file('tests/test_data/reader/test_date')
         assert file == 'nginx-access-ui.log-20210202.gz'
 
     def test_empty_dir(self):
-        reader = LogReader('tests/test_data/reader')
-        file = reader.find_latest_file()
-        assert file == ''
+        file, date = find_latest_file('tests/test_data/reader')
+        assert file is None
 
     def test_unknown_format(self):
-        reader = LogReader('tests/test_data/reader/test_unknown_format')
-        file = reader.find_latest_file()
-        assert file == ''
+        file, date = find_latest_file('tests/test_data/reader/test_unknown_format')
+        assert file is None
 
 
 if __name__ == '__main__':
